6b41bd4950199cc6f9e8d2cc314ed10b9204e447
5831013 <z5831013@gmail.com>
2015-12-22 00:06:48 +0800

    st1

---
+++ src/test/main.java
@@ -0,0 +1,18 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class Question {
+		   String description;
+		   public Question(String desc) {
+		      this.description = desc;
+		   }
+		   public void print() {
+		      System.out.println(description);
+		   }      
+		}
+}
